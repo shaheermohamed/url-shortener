7812804ae3a901ae667c0b81f167b7194b304bc8
Mohamed Shaheer <shaheer@coderythm.com>
2024-06-03 21:03:33 +0400

    feat: updated server url

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ mongoose.connection.on("error", (err) => {
 
 app.use("/api", urlRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
